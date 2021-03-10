a2912e69e54d28e44732986e0dd196b657506fa8
Alvaro <alvaro.h.z1998@gmail.com>
2021-03-10 16:51:31 -0400

    convocatoria1

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import {Convocatoria} from '../../models/convocatoria';
+import { ConvocatoriaService } from "../../services/convocatoria.service";
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  convocatoriaArray: Convocatoria[] = [
+  ];
+
+  convito = new Convocatoria();
+
+
+
+  constructor(private convocatoriaService: ConvocatoriaService) { }
 
   ngOnInit(): void {
+    
+    this.convocatoriaService.convocatorias().subscribe(convoc => this.convocatoriaArray = convoc)
+    this.convito = new Convocatoria();
+  }
+
+  saveNew(form: NgForm){
+
+    this.convocatoriaService.addNewConvocatoria(this.convito).subscribe(response =>{
+      console.log(response);
+      console.log(this.convito);
+    },error =>{console.log(<any>error)
+    })
+    form.resetForm(this);
+
   }
 
 }
